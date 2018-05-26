20146521f6d2f2e6ad114a472c2c4c2b3ae4d074
Kyle Kelley <rgbkrk@gmail.com>
2018-05-25 22:01:34 -0700

    here we go

--- index.js
+++ index.js
@@ -13,22 +13,49 @@ var messaging = require("@nteract/messaging");
 // Standard file system yo
 var fs = require("fs");
 
+// The dash that is low
+var _ = require("lodash");
+
+var chalk = require("chalk");
+var treeify = require("treeify");
+
 // Neat trick to sleep in an async/await setup
 // > await sleep(100)
 function sleep(duration) {
   return new Promise(resolve => setTimeout(resolve, duration));
 }
 
-// Let's launch a kernel, check its info, and execute code
-async function main() {
-  var { spec } = await kernelspecs.find("python3");
+const yargs = require("yargs");
 
+async function main() {
+  var ks = await kernelspecs.findAll();
+
+  var argv = require("yargs")
+    .default("kernel", function randomKernel() {
+      return _.sample(ks).name;
+    })
+    .alias("k", "kernel")
+    .describe("k", "the kernel to run")
+    .nargs("k", 1).argv;
+
+  const kernelName = argv.kernel;
+
+  console.log(kernelName);
+
+  await runKernel(ks[kernelName].spec);
+}
+
+// Let's launch a kernel, check its info, and execute code
+async function runKernel(kernelSpec) {
   // Launch that kernel and get everything you need for cleanup
-  var { config, spawn, connectionFile } = await spawnteract.launchSpec(spec, {
-    cwd: "/tmp",
-    // No STDIN, opt in to STDOUT and STDERR as node streams
-    stdio: ["ignore", "pipe", "pipe"]
-  });
+  var { config, spawn, connectionFile } = await spawnteract.launchSpec(
+    kernelSpec,
+    {
+      cwd: "/tmp",
+      // No STDIN, opt in to STDOUT and STDERR as node streams
+      stdio: ["ignore", "pipe", "pipe"]
+    }
+  );
 
   console.log("Connection File: ", connectionFile);
 
@@ -42,12 +69,25 @@ async function main() {
     console.log("KERNEL STDERR: ", text);
   });
 
+  spawn.on("close", code => {
+    if (code) {
+      console.log("closed early and poorly", code);
+      process.exit(code);
+    }
+  });
+
+  //// OH SNAP, if it fails to launch above here we are waiting forever
   // Set up an Rx Subject to send and receive Jupyter messages
   var channel = await enchannel.createMainChannel(config);
 
   // Log every message we get back from the kernel
   var subscription = channel.subscribe(
-    x => console.log(x),
+    msg => {
+      console.log(chalk.bold(msg.header.msg_type));
+      console.log(
+        treeify.asTree(_.omit(msg, ["buffers", "parent_header"]), true)
+      );
+    },
     err => console.error(err)
   );
 
@@ -59,7 +99,7 @@ async function main() {
   channel.next(messaging.kernelInfoRequest());
 
   // BIG DATA
-  channel.next(messaging.executeRequest("print(2 + 2)"));
+  channel.next(messaging.executeRequest("2 + 2"));
 
   // 😴  sleep so that our BIG DATA computation finishes
   await sleep(400);
